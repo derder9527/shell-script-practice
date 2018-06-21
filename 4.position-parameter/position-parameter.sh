018766158a0779d4bdb1aff51cdbf1a9fc5ee713
derder <derder9527@gmail.com>
2018-06-21 13:43:41 +0800

    4.position-parameter

---
+++ 4.position-parameter/position-parameter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#輸出第零個parameter(腳本的名稱)
+echo '$0='$0
+#輸出第1個parameter
+echo '$1='$1
+#輸出第2個parameter 
+echo '$2='$2
+#輸出第3個parameter 
+echo '$3='$3
+#輸出第4個parameter 
+echo '$4='$4
+#輸出全部的parameter 
+echo '$*='$*
+#輸出全部的parameter
+echo '$@='$@
+#輸出parameter的個數
+echo '$#='$#
+
---
+++ 4.position-parameter/position-parameter_set.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#直接設定parameter
+set one two three four
+#輸出第零個parameter(腳本的名稱)
+echo '$0='$0
+#輸出第1個parameter
+echo '$1='$1
+#輸出第2個parameter 
+echo '$2='$2
+#輸出第3個parameter 
+echo '$3='$3
+#輸出第4個parameter 
+echo '$4='$4
+#輸出全部的parameter 
+echo '$*='$*
+#輸出全部的parameter
+echo '$@='$@
+#輸出parameter的個數
+echo '$#='$#
