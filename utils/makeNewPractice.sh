112d1beb390a3197a5467cede982a8b8d3d3be09
derder <derder9527@gmail.com>
2018-07-06 12:55:42 +0800

    add utils

---
+++ utils/makeNewPractice.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#取上層目錄數字開頭的目錄數+1就是新的目錄次序
+sequence=$(echo `ls .. | grep '^[0-9]' |wc -l `+1 |bc)
+echo  "這是第$sequence 個practice，請輸入新的名稱"
+read practiceName
+folder=$sequence.$practiceName
+mkdir ../"$folder"
+if [ $? ]
+then
+  echo "資料夾：$folder 建立成功"
+  touch ../$folder/$practiceName.sh
+  echo "$practiceName.sh 建立成功"
+  touch ../$folder/$practiceName"_demo.gif"
+  echo "無內容gif：$practiceName"_demo.gif" 建立成功"
+  touch ../$folder/README.md
+  echo  "\n\n\n" >>../$folder/README.md
+  echo "![$practiceName](https://raw.githubusercontent.com/derder9527/shell-script-practice/master/$folder/$practiceName"_demo.gif")">>../$folder/README.md
+  echo  "\n\n\n" >>../$folder/README.md
+  echo "[Source code at GitHub](https://github.com/derder9527/shell-script-practice/tree/master/$folder)">>../$folder/README.md
+  echo "README.md 建立成功"
+else
+  echo "資料夾：$folder 建立失敗！"
+  exit
+fi
